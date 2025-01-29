1b5f6ceec2eff00ef556d2056178394ad513a4d7
sebastienDOC <s.dechand21@gmail.com>
2025-01-29 13:13:10 +0100

    feat: ajout et mise en place du footer
    TODO: ajustement du style et du contenu

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
-  standalone: true
+  standalone: true,
 })
 export class FooterComponent {
-
+  currentYear: number = new Date().getFullYear();
 }
