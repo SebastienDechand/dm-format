46f34f2291e721fd93182a8d32cc24903527e2eb
sebastienDOC <s.dechand21@gmail.com>
2025-01-29 12:50:28 +0100

    feat: ajout et mise en place de la page de conditions d'organisation
    TODO: étoffer et ajuster le style

--- src/app/pages/organisation/organisation.component.ts
+++ src/app/pages/organisation/organisation.component.ts
@@ -1,11 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-organisation',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './organisation.component.html',
-  styleUrl: './organisation.component.scss'
+  styleUrl: './organisation.component.scss',
 })
-export class OrganisationComponent {
-
-}
+export class OrganisationComponent {}
