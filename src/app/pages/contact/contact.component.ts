68bb9349d9c6cfd1cddd663606e38bd9f55674d7
sebastienDOC <s.dechand21@gmail.com>
2025-02-01 17:25:06 +0100

    feat: ajotu et mise en place de la page de contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss'],
+  imports: [FormsModule, CommonModule, MatSnackBarModule],
 })
 export class ContactComponent {
+  contactData = {
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  };
 
+  constructor(private snackBar: MatSnackBar) {}
+
+  onSubmit() {
+    if (
+      this.contactData.name &&
+      this.contactData.email &&
+      this.contactData.message
+    ) {
+      console.log('Formulaire soumis :', this.contactData);
+
+      this.snackBar.open('Votre message a bien été envoyé !', 'Fermer', {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        panelClass: ['success-snackbar'],
+      });
+
+      this.contactData = { name: '', email: '', phone: '', message: '' };
+    }
+  }
 }
