d70b615e5dfe1d8fc143130019ba69833e28326d
sebastienDOC <s.dechand21@gmail.com>
2025-02-18 14:26:55 +0100

    wip: ajout de l'editMode sur la page organisation

--- src/app/pages/organisation/organisation.component.ts
+++ src/app/pages/organisation/organisation.component.ts
@@ -1,25 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../services/api.service';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { EditButtonComponent } from '../../components/edit-button/edit-button.component';
 import { ConditionsData } from '../../models/organisation.models';
+import { AdminService } from '../../services/admin.service';
+import { ApiService } from '../../services/api.service';
+import { EditModeService } from '../../services/edit-mode.service';
 import { SafeHtmlPipe } from '../../pipes/safe-html.pipe';
 
 @Component({
   selector: 'app-organisation',
-  imports: [CommonModule, SafeHtmlPipe],
+  standalone: true,
+  imports: [CommonModule, FormsModule, EditButtonComponent, SafeHtmlPipe],
   templateUrl: './organisation.component.html',
   styleUrl: './organisation.component.scss',
 })
 export class OrganisationComponent implements OnInit {
-  organisationData!: ConditionsData;
+  private apiService: ApiService = inject(ApiService);
+  private adminService: AdminService = inject(AdminService);
+  private editModeService: EditModeService = inject(EditModeService);
 
-  constructor(private apiService: ApiService) {
-    this.apiService.getOrganisation().subscribe((data) => {
-      this.organisationData = data;
-    });
-  }
+  organisationData!: ConditionsData;
+  isAdmin$: Observable<boolean> = this.adminService.isAdminMode$;
+  editMode: { [key: string]: boolean } = {};
+  private destroy$ = new Subject<void>();
 
   ngOnInit(): void {
+    this.editModeService.editMode$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((editMode) => {
+        this.editMode = editMode;
+      });
+
     this.apiService.getOrganisation().subscribe(
       (data) => {
         this.organisationData = data;
@@ -28,5 +41,29 @@ export class OrganisationComponent implements OnInit {
         console.error('Error fetching organisation page data', error);
       }
     );
+  }
+
+  toggleEditMode(field: string) {
+    if (this.isAdmin$) {
+      this.editModeService.toggleEditMode(field);
+    }
+  }
+
+  saveChanges() {
+    this.apiService.patchOrganisation(this.organisationData).subscribe(
+      (data) => {
+        this.organisationData = data;
+        this.editModeService.resetEditModes();
+        alert('Modifications sauvegardées avec succès');
+      },
+      (error) => {
+        console.error('Error saving organisation page data', error);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
