8f45b3134884a330b9bf01a7ed75b5cf3770c65b
sebastienDOC <s.dechand21@gmail.com>
2025-03-16 10:44:45 +0100

    feat: ajout d'un bouton caché pour faire apparaitre le login admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,16 @@ import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AdminToggleComponent } from './components/admin-toggle/admin-toggle.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    AdminToggleComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
