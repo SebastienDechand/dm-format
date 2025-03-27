85bdb51c531240066a308f2005423380ac849baa
sebastienDOC <s.dechand21@gmail.com>
2025-03-27 18:05:06 +0100

    fix: prod

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 import { About } from '../models/about.models';
 import { ContactData } from '../models/contact.models';
 import { GalleryImage } from '../models/gallery.models';
